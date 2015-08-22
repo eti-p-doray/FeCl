87fba120203a88baa0ad59bcebdabc7ae27cef59
Etienne Pierre-Doray <etipdoray@gmail.com>
2015-08-21 20:31:36 -0400

    correct matlab test

--- +fec/+test/all.m
+++ +fec/+test/all.m
@@ -3,7 +3,7 @@ function success = all
     trellis = poly2trellis(4, [13, 15], 13);
     turboTrellis = poly2trellis(4, [15], 13);
     code{1} = fec.ConvolutionalCode(trellis, T);
-    code{2} = fec.TurboCode({turboTrellis, turboTrellis}, {randperm(T), randperm(T)}, fec.TrellisEndType.Truncation, 5, fec.StructureType.Parallel);
+    code{2} = fec.TurboCode({turboTrellis, turboTrellis}, {randperm(T), randperm(T)}, fec.TrellisEndType.Truncation, 5, fec.SchedulingType.Parallel);
     code{3} = fec.LdpcCode(dvbs2ldpc(1/2));
 
     success = true;
